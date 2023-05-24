36889b7482cc3daae2d0863be112870eacfb7b70
Red10ne <redboukri@gmail.com>
2023-05-24 16:24:52 -0700

    fix code

--- _atoi.c
+++ _atoi.c
@@ -1,8 +1,8 @@
 #include "shell.h"
 
 /**
- * interactive - returns true if shell is interactive mode
- * @info: struct address
+ * interactive - returns true if shell is interactive mode.
+ * @info: struct addr.
  *
  * Return: 1 if interactive mode, 0 otherwise
  */
@@ -12,10 +12,10 @@ int interactive(info_t *info)
 }
 
 /**
- * is_delim - checks if character is delimeter
- * @c: Char to be check
- * @delim: delimeter string
- * Return: 1 if true, 0 if false
+ * is_delim - checks if char is a delimeter.
+ * @c: the char to check.
+ * @delim: the delimeter str.
+ * Return: 1 if true, 0 if false.
  */
 int is_delim(char c, char *delim)
 {
@@ -26,9 +26,9 @@ int is_delim(char c, char *delim)
 }
 
 /**
- *_isalpha - checks for an  alphabetic character
- *@c: character to an input
- *Return: 1 if c is an alphabetic, 0 if  otherwise
+ * _isalpha - checks for alphabetic char.
+ * @c: The char to input.
+ * Return: 1 if c is alphabetic, 0 otherwise.
  */
 
 int _isalpha(int c)
@@ -40,9 +40,9 @@ int _isalpha(int c)
 }
 
 /**
- *_atoi - converts string to integer
- *@s: String to be converted
- *Return: 0 if no numbers in the string, converted to  number otherwise
+ * _atoi - converts a str to an int.
+ * @s: the string to be converted.
+ * Return: 0 if no num in str, converted num otherwise.
  */
 
 int _atoi(char *s)
@@ -50,7 +50,7 @@ int _atoi(char *s)
 	int i, sign = 1, flag = 0, output;
 	unsigned int result = 0;
 
-	for (i = 0;  s[i] != '\0' && flag != 2; i++)
+	for (i = 0; s[i] != '\0' && flag != 2; i++)
 	{
 		if (s[i] == '-')
 			sign *= -1;
